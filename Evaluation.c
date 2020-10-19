e832af85bf51114509f3b318f1f3516a926d1733
gilles <gilles@Muff>
2020-10-19 18:38:42 +0200

    implemntation of simple command

--- Evaluation.c
+++ Evaluation.c
@@ -8,13 +8,20 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+
+/*
+ * Will create a child processus to execute a command 
+ * with execvp with the command and args
+ * passed in parameters
+ * */
 int execute_simple_command(char *command, char **args){
 	int pid  = fork();
-	if(!command){
+	if(!pid){
 		int e = execvp(command, args);
 		return e;	
 	}else{
-		return pid;
+		wait(NULL);
+		return 1;
 	}
 }
 
@@ -25,18 +32,14 @@ int evaluer_expr(Expression *e)
   if (e->type == VIDE)
     return 0;
   if(e->type == SIMPLE){
-	int command_to_execute = fork();
-	if(!command_to_execute){
-		execvp(e->arguments[0], e->arguments);		
-	}
-	wait(NULL);
-	return EXIT_SUCCESS;
+	  execute_simple_command(e->arguments[0], e->arguments);
+	  return 1;
   }
 
-  if(e->type == SEQUENCE_ET){
-	
+  /*if(e->type == SEQUENCE_ET){
+		  
  	 
-  }
+  }*/
   fprintf(stderr, "not yet implemented \n");
   return 1;
 }
