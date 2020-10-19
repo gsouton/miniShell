2deda3e5cf0a837652baa3124cded6ba9431a70d
Gilles <gsouton@gmail.com>
2020-10-19 20:32:36 +0200

    Add ; && and || to shell

--- Evaluation.c
+++ Evaluation.c
@@ -7,10 +7,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-
+#include <errno.h>
 
 /*
- * Will create a child processus to execute a command 
+ * Will create a child processus to execute a command
  * with execvp with the command and args
  * passed in parameters
  * */
@@ -18,10 +18,11 @@ int execute_simple_command(char *command, char **args){
 	int pid  = fork();
 	if(!pid){
 		int e = execvp(command, args);
-		return e;	
+		return e; // in case of invalid command zombie will be created
 	}else{
-		wait(NULL);
-		return 1;
+		int wstatus;
+		int w = waitpid(pid, &wstatus, 0 );
+		return wstatus;
 	}
 }
 
@@ -29,17 +30,43 @@ int execute_simple_command(char *command, char **args){
 
 int evaluer_expr(Expression *e)
 {
-  if (e->type == VIDE)
-    return 0;
-  if(e->type == SIMPLE){
-	  execute_simple_command(e->arguments[0], e->arguments);
-	  return 1;
-  }
+	if (e->type == VIDE)
+		return 0;
 
-  /*if(e->type == SEQUENCE_ET){
-		  
- 	 
-  }*/
-  fprintf(stderr, "not yet implemented \n");
-  return 1;
+	if(e->type == SIMPLE){
+		int ret = execute_simple_command(e->arguments[0], e->arguments);
+		return ret;
+	}
+
+	else if(e->type == SEQUENCE){
+		evaluer_expr(e->gauche);
+		evaluer_expr(e->droite);
+		return 0;
+	}
+	
+	else if(e->type == SEQUENCE_ET){
+		int ret = 0;
+		if((ret = evaluer_expr(e->gauche)) == 0){
+			if((ret = evaluer_expr(e->droite)) == 0){
+				return 0;
+			}
+		}
+		return ret;
+	}
+
+	else if(e->type == SEQUENCE_OU){
+		int ret = 0;
+		if((ret = evaluer_expr(e->gauche) == 0)){
+			return ret;
+		}
+		else if( (ret = evaluer_expr(e->droite)) == 0){
+			return ret;
+		}
+		return ret;
+	}
+
+
+
+	fprintf(stderr, "not yet implemented \n");
+	return 1;
 }
