f57daf73631460e7e261aa708ab1011de8042741
Gilles <gsouton@gmail.com>
2020-10-26 17:24:39 +0100

    Kill zombie to every call of evaluer

--- Evaluation.c
+++ Evaluation.c
@@ -10,6 +10,23 @@
 #include <fcntl.h>
 #include <errno.h>
 
+void check(int cond, char *msg){
+	if(!cond){
+		perror(msg);
+	}
+}
+
+void kill_zombies(){
+	int wstatus;
+	int w = waitpid(-1, &wstatus, WNOHANG);
+	if(w > 0){
+		wait(NULL);
+		fprintf(stderr, "%d, Fini\n", w);
+	}
+
+
+}
+
 /*
  * Will create a child processus to execute a command
  * with execvp with the command and args
@@ -25,8 +42,9 @@ int execute_simple_command(char *command, char **args, bool background){
 		if(!background){
 			waitpid(pid, &wstatus, 0);
 		}else{
-			waitpid(pid, &wstatus, WNOHANG); 
-			fprintf(stderr, "process executed in background wait satus = %d\n", wstatus);
+			//int w = waitpid(pid, &wstatus, WNOHANG);
+			//check(w >= 0, "waitpid");
+			fprintf(stderr, "Process executed in background %d\n",pid);
 		}
 		return wstatus;
 	}
@@ -36,23 +54,19 @@ int execute_simple_command(char *command, char **args, bool background){
 int evaluer_expr_background(Expression *e){
 	if(e->type == SIMPLE){
 		int ret = execute_simple_command(e->arguments[0], e->arguments, true);
-		if(WIFEXITED(ret)){
-			fprintf(stderr, "Child exited normally ");
-			wait(NULL);
-		}
 		return ret;
 	}
 	if(e != NULL){
 		int ret = evaluer_expr(e->gauche);
 		ret = evaluer_expr_background(e->droite);
-		
+
 		return ret;
 	}
 
 	fprintf(stderr, "Not implemented yet !\n");
 	return 1;
 }
-			
+
 
 
 
@@ -61,21 +75,23 @@ int evaluer_expr(Expression *e)
 	if(e == NULL){
 		return 1;
 	}
+	kill_zombies();
+
 	if (e->type == VIDE){
 		return 0;
 	}
 
 	if(e->type == SIMPLE){
-		int ret = execute_simple_command(e->arguments[0], e->arguments,false ); // for simple execute command 
+		int ret = execute_simple_command(e->arguments[0], e->arguments,false ); // for simple execute command
 		return ret; // return status of executing the command
 	}
 
 	else if(e->type == SEQUENCE){
 		int ret = evaluer_expr(e->gauche); // first evaluating left
-		ret = evaluer_expr(e->droite); // then evaluate right 
-		return ret; //status 
+		ret = evaluer_expr(e->droite); // then evaluate right
+		return ret; //status
 	}
-	
+
 	else if(e->type == SEQUENCE_ET){
 		int ret = 0;
 		if((ret = evaluer_expr(e->gauche)) == 0){ // if the evaluation of left has no error (means returned 0)
@@ -111,7 +127,7 @@ int evaluer_expr(Expression *e)
 
 	fprintf(stderr, "not yet implemented \n");
 	return 1;
-	
-	
+
+
 
 }
